59f64dab01b1699f07981db822bdbca5430b415b
Gijs Verdonk <g.verdonk@icloud.com>
2024-03-13 12:01:05 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Boodschappenlijst</h1>
+<p>Dit is om de API te testen.</p>
+
+<script>
+    const getItems = async () => {
+        const res = await fetch('http://localhost:5006/api/Item');
+        const itemJson = await res.json();
+        console.log("Button clicked...");
+        console.log(itemJson);
+    }
+
+    getItems();
+</script>
+
